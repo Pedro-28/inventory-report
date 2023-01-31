b02d06a791f4293e10d14eb1333a32037ce227b6
Pedro-28 <pedrohdp2017@gmail.com>
2023-01-31 15:44:41 -0300

    Requisito 01 concluido

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 1
+    nome_do_produto = "Leite"
+    nome_da_empresa = "Nestle"
+    data_de_fabricacao = "31/01/2023"
+    data_de_validade = "20/03/2023"
+    numero_de_serie = "123456789"
+    instrucoes_de_armazenamento = "Manter na geladeira após aberto"
+
+    product = Product(
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento
+        )
+
+    assert product.id == id
+    assert product.nome_do_produto == nome_do_produto
+    assert product.nome_da_empresa == nome_da_empresa
+    assert product.data_de_fabricacao == data_de_fabricacao
+    assert product.data_de_validade == data_de_validade
+    assert product.numero_de_serie == numero_de_serie
+    assert product.instrucoes_de_armazenamento == instrucoes_de_armazenamento
