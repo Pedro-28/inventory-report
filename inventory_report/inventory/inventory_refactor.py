a4fd26d0441b3ec941ef2f563341882fc6543f87
Pedro-28 <pedrohdp2017@gmail.com>
2023-02-01 17:29:04 -0300

    Requisito 10 concluido

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,4 +1,5 @@
 from inventory_report.importer.importer import Importer
+from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 
 from inventory_report.reports.complete_report import CompleteReport
@@ -8,11 +9,15 @@ from inventory_report.reports.simple_report import SimpleReport
 
 class InventoryRefactor:
     def __init__(self, importer: Importer):
-        self.__importer = importer
+        self.importer = importer
+        self.data = list()
 
     def import_data(self, path: str, report_type: str):
-        reports = self.__importer.import_data(path)
+        self.data.extend(self.importer.import_data(path))
         if report_type == "simples":
-            return SimpleReport.generate(reports)
+            return SimpleReport.generate(self.data)
         elif report_type == "completo":
-            return CompleteReport.generate(reports)
+            return CompleteReport.generate(self.data)
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
